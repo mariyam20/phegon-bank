4bc6cc2476ad1ad848b4b519456210b030016c2d
khanarshi <khanarshi3066@gmail.com>
2026-05-14 21:02:35 +0530

    connected to database

---
+++ src/main/java/com/phegon/phegonbank/PhegonBankApplication.java
@@ -0,0 +1,13 @@
+package com.phegon.phegonbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhegonBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhegonBankApplication.class, args);
+	}
+
+}
